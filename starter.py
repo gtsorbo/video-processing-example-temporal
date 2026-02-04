2057f798a157cd39571277404b0031dbad023f34
Grant Sorbo <gtsorbo@mac.com>
2026-02-03 18:23:21 -0600

    accept command-line args for filepath

--- starter.py
+++ starter.py
@@ -3,12 +3,18 @@ import uuid
 from temporalio.client import Client
 
 from shared import InputVideo
+import sys
 
 async def main():
+    if len(sys.argv) < 2:
+        print("Usage: python starter.py <filepath>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
     client = await Client.connect("localhost:7233")
     result = await client.execute_workflow(
         "VideoProcessingWorkflow",
-        InputVideo(filepath="/Users/Grant/Desktop/VideoTest/TDWP2.mp4"),
+        InputVideo(filepath=filepath),
         id=f"video-processing-workflow-{uuid.uuid4()}",
         task_queue="video-processing-queue",
     )
